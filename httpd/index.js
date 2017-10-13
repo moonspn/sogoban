7a60544ec638cabf350fba874e68c8cbc36384ca
moonspn <moonspn1094@gmail.com>
2017-10-13 15:25:52 +0800

    加入 index.html 檔案

---
+++ httpd/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.createServer((request, response) => {
+let fs = require('fs')
+
+fs.readFile('index.html', (err, data) => {
+response.writeHead(200, {
+'Content-Type': 'text/html'
+});
+response.write(data);
+response.end();
+});
+}).listen(8081);
+
+// log message to Console
+console.log('Server running at http://127.0.0.1:8081/');
+
+// index.js
