c4a3a315f8384bda7bf074ee5e410e90e7c53b6c
moonspn <moonspn1094@gmail.com>
2017-11-03 14:12:57 +0800

    ...

---
+++ httpd/js/index.js
@@ -0,0 +1,22 @@
+ 'use strict';
+
+ let http = require('http');
+
+ http.createServer((request, response) => { request.on('end', () => {
+ console.log('Request method: ' + request.method);
+ console.log('Request url: ' + request.url);
+ });
+
+ // 傳送 HTTP header
+ // HTTP Status: 200 : OK
+ // Content Type: text/plain
+ response.writeHead(200, {
+ 'Content-Type': 'text/plain'
+ });
+
+ // 傳送回應內容。
+ response.end('Hello World!\n');
+ }).listen(8088);
+
+ // log message to Console
+ console.log('Server running at http://127.0.0.1:8088/');
