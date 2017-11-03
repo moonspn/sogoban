1877217bbc18e2550593b0ced8c589415290a98c
moonspn <moonspn1094@gmail.com>
2017-11-03 15:38:03 +0800

    ...

--- htdocs/js/Node.js
+++ htdocs/js/Node.js
@@ -1,21 +1,21 @@
- 'use strict';
+'use strict';
 
- let http = require('http');
+let http = require('http');
 
- http.createServer((request, response) => {
- // 取得 node.js 的 fs 模組
- let fs = require('fs')
+http.createServer((request, response) => {
+  // 取得 node.js 的 fs 模組
+  let fs = require('fs')
 
- fs.readFile('../htdocs/index.html', (err, data) => {
- response.writeHead(200, {
- 'Content-Type': 'text/html'
- });
+  fs.readFile('../htdocs/index.html', (err, data) => {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    
+    response.write(data);
 
- response.write(data);
+    response.end();
+  });
+}).listen(8088);
 
- response.end();
- });
- }).listen(8088);
-
- // log message to Console
- console.log(' 伺服器啟動，連線 url: http://127.0.0.1:8088/');
+// log message to Console
+console.log(' 伺服器啟動，連線 url: http://127.0.0.1:8088/');
